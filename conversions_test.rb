b562d0b3ac825e5af8016dfd7f2e0c5f1da478e1
James Bridges <masterbridges@gmail.com>
2019-11-26 18:22:29 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,17 +1,17 @@
 require_relative './conversions.rb'
 
 puts "Given 0, it should return 0.0"
-answer = ounces_to_grams(0)
-if answer == 0.0
+result = ounces_to_grams(0)
+if result == 0.0
   puts "Passed!"
 else
-  puts "Test failed: got #{answer} instead"
+  puts "Test failed: got #{result} instead"
 end
 
 puts "Given 1, it should return 28.3495"
-answer = ounces_to_grams(1)
-if answer == 28.3495
+result = ounces_to_grams(1)
+if result == 28.3495
   puts "Passed!"
 else
-  puts "Test failed: got #{answer} instead"
+  puts "Test failed: got #{result} instead"
 end
