0c4a4d63802ef8b5141f5bc2cfa0821cff8e776f
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-11 00:22:51 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,8 +1,8 @@
 require_relative './conversions.rb'
 
-puts "Given 0, it should return 0.0"
-answer = ounces_to_grams(0)
-if answer == 0.0
+puts "Given 1, it should return 1.1"
+answer = ounces_to_grams(1)
+if answer == 1.1
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
