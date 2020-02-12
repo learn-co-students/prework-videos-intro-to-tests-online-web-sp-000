6695bbe32b2c4bc1250104cbe5344f8fa0ee8394
Alvin Coulson <aaacoulson@gmail.com>
2020-02-12 10:41:43 -0500

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,5 +1,21 @@
 require_relative './conversions.rb'
 
+# puts "ounce to grams, when given 0, return 0.0"
+# answer = ounces_to_grams(0)
+# if answer == 0.0
+#   puts "Passed!"
+# else
+#   puts "Test Failed: got #{answer} instead"
+# end
+
+# puts "ounce to grams, when given 1, return 28.3495"
+# answer = ounces_to_grams(1)
+# if answer == 28.3495
+#   puts "Passed!"
+# else
+#   puts "Test Failed: got #{answer} instead"
+# end
+
 puts "Given 0, it should return 0.0"
 answer = ounces_to_grams(0)
 if answer == 0.0
