d282ca501b6f46510f706f92dd11dd8f08281d41
YUKIKO IKEDA <applelele@gmail.com>
2020-06-07 23:43:55 +0000

    Done.

--- conversions.rb
+++ conversions.rb
@@ -2,3 +2,8 @@ GRAMS_PER_OUNCE = 28.3495
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
+
+def meal_choice(veg, starch, protain="meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protain} with #{veg} and #{starch}."
+end
