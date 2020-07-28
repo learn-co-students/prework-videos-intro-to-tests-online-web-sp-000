f4c009d9d02a1994b2954682ed0e713f6d65373b
Glenn Jensen <g.e.jensen@gmail.com>
2020-07-28 16:57:48 +0000

    Done.

---
+++ meal_choice.rb
@@ -0,0 +1,9 @@
+# meal choice labs
+def meal_choice(vegetable, starch, protien="meat")
+
+meal = "A plate of #{protien} with #{vegetable} and #{starch}."
+puts "What a nutritious meal!"
+puts meal
+meal
+
+end
