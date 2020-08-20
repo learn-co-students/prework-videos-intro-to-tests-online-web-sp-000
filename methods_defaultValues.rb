569786d6d13f45f22542ed34cd88d020492bdc10
kathinka martinsen <kathinkamartinsen@gmail.com>
2020-08-20 21:47:09 +0000

    Done.

---
+++ methods_defaultValues.rb
@@ -0,0 +1,6 @@
+def meal_choice(veg="broccoli", starch="macaroni", protein="meat")
+  puts ("What a nutritious meal!")
+  puts ("A plate of #{protein} with #{veg} and #{starch}.")
+  return  "A plate of #{protein} with #{veg} and #{starch}."
+
+end
