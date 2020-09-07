d4e71261249d4b2f4e1d8a303fe43ca3e20e9b75
juan siu <juan.siu@gmail.com>
2020-09-07 23:59:29 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ RSpec.configure do |config|
   config.failure_color = :magenta # or whateer color you like
   config.success_color = :cyan
 
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
