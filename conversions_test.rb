e76e6f7817e79620829c80aba92ce133aea04050
Becky Rosa <becky_m_rosa@yahoo.com>
2020-09-22 03:34:37 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,8 +1,8 @@
 require_relative './conversions.rb'
 
-puts "Given 0, it should return 0.0"
+puts "Given 3, it should return 3.0"
 answer = ounces_to_grams(0)
-if answer == 0.0
+if answer == 3.0
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
