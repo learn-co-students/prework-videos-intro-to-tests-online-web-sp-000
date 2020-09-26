6979c1d28bbe568b93df55e67d46d7803ef2d483
Jjgarcilazo <jjgarcilazo@gmail.com>
2020-09-26 12:55:15 -0700

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,8 +1,8 @@
 require_relative './conversions.rb'
 
 puts "Given 0, it should return 0.0"
-answer = ounces_to_grams(0)
-if answer == 0.0
+answer = ounces_to_grams(ss)
+if answer == 22
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
