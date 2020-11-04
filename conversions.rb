bc8ffc58922dcb53cfaf70a69933654125569918
Keith Filkins <kafilkins@me.com>
2020-11-04 20:36:58 +0000

    Done.

--- conversions.rb
+++ conversions.rb
@@ -2,3 +2,9 @@ GRAMS_PER_OUNCE = 28.3495
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
+
+INCHES_PER_FOOT = 12
+
+def ounces_to_grams(ounces)
+  ounces.to_f * INCHES_PER_FOOT
+end
--- conversions_test.rb
+++ conversions_test.rb
@@ -1,17 +1,24 @@
 require_relative './conversions.rb'
 
-puts "Given 0, it should return 0.0"
-answer = ounces_to_grams(0)
-if answer == 0.0
+puts "Given 1, it should return 1.0"
+answer = ounces_to_grams(1)
+if answer == 1.0
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
 end
 
 puts "Given 1, it should return 28.3495"
-answer = ounces_to_grams(1)
+answer = ounces_to_grams(2)
 if answer == 28.3495
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
+end
+puts "Given 10, it should return 10.0"
+answer = ounces_to_grams(10)
+if answer == 10
+  puts "Passed!"
+else
+  puts "Test Failed: got #{answer} instead"
 end
